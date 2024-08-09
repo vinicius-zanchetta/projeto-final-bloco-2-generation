ad22ae807f9550edd310878db9d60a18435bc4e0
vinicius zanchetta <viniciuszanchettadev@gmail.com>
2024-08-09 11:54:59 -0300

    Alterado DevService para ProdService, para rodar conexao com BD remotamente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DevService } from './auth/data/dev.service';
 import { CategoriaModule } from './categorias/categoria.module';
 import { ProdutoModule } from './produtos/produto.module';
 import { AuthModule } from './auth/auth.module';
 import { UsuarioModule } from './usuarios/usuario.module';
+import { ProdService } from './auth/data/prod.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
-      useClass: DevService,
+      useClass: ProdService,
       imports: [ConfigModule],
     }),
     CategoriaModule,
